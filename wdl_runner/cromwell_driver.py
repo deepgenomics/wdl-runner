deepgenomics/wdl-runner

commit a2f323df3f8e1e2b07b86c2a0237b9eb0cc57d20

fix import for tests

wdl_runner/file_util.py
@@ -16,7 +16,7 @@ import subprocess
 from googleapiclient import discovery
 from googleapiclient.errors import HttpError
 
-import sys_util
+import wdl_runner.sys_util
 
 
 def file_safe_substitute(file_name, mapping):
@@ -58,7 +58,7 @@ def verify_gcs_dir_empty_or_missing(path):
 
   # Verify the input is a GCS path
   if not path.startswith('gs://'):
-    sys_util.exit_with_error("Path is not a GCS path: '%s'" % path)
+    wdl_runner.sys_util.exit_with_error("Path is not a GCS path: '%s'" % path)
 
   # Tokenize the path into bucket and prefix
   parts = path[len('gs://'):].split('/', 1)
@@ -85,7 +85,7 @@ def verify_gcs_dir_empty_or_missing(path):
       error = simplejson.loads(err.content)
       error = error['error']
 
-      sys_util.exit_with_error(
+      wdl_runner.sys_util.exit_with_error(
           "%s %s: '%s'" % (error['code'], error['message'], path))
 
     items.extend(response.get('items', []))

commit eca2e741ae3cdfbebd882da1c3596d9f176bd7eb

fix: import errors
imports were broken in a2f323df

wdl_runner/cromwell_driver.py
@@ -18,10 +18,10 @@ import logging
 import os
 import subprocess
 import time
+from typing import List
 
 import requests
-from wdl_runner import sys_util
-from typing import List
+import sys_util
 
 
 class CromwellDriver(object):
@@ -42,9 +42,7 @@ class CromwellDriver(object):
         if self.jvm_flags is not None:
             jvm_args += self.jvm_flags
         jvm_args += ["-jar", self.cromwell_jar]
-        self.cromwell_proc = subprocess.Popen(
-            ["java"] + jvm_args + ["server"]
-        )
+        self.cromwell_proc = subprocess.Popen(["java"] + jvm_args + ["server"])
 
         logging.info("Started Cromwell")
 

wdl_runner/file_util.py
@@ -15,12 +15,11 @@ from typing import List
 from urllib.parse import urlparse
 
 import simplejson
+import sys_util
 from google.cloud import storage
 from google.cloud.exceptions import GoogleCloudError
 from googleapiclient import discovery
 from googleapiclient.errors import HttpError
-
-import wdl_runner.sys_util
 
 
 def file_safe_substitute(file_name, mapping):
@@ -107,7 +106,7 @@ def verify_gcs_dir_empty_or_missing(path):
 
     # Verify the input is a GCS path
     if not path.startswith("gs://"):
-        wdl_runner.sys_util.exit_with_error("Path is not a GCS path: '%s'" % path)
+        sys_util.exit_with_error("Path is not a GCS path: '%s'" % path)
 
     # Tokenize the path into bucket and prefix
     parts = path[len("gs://") :].split("/", 1)
@@ -135,7 +134,7 @@ def verify_gcs_dir_empty_or_missing(path):
             error = simplejson.loads(err.content)
             error = error["error"]
 
-            wdl_runner.sys_util.exit_with_error(
+            sys_util.exit_with_error(
                 "%s %s: '%s'" % (error["code"], error["message"], path)
             )
 